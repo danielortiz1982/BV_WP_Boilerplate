1f0e726f47623682d43f798fd873bd10f846c6e6
dortiz <daniel.ortiz@teachforamerica.org>
2018-08-30 12:26:16 -0400

    update to BV Boilerplate

--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ function BV_Boilerplate_Widgets_Init(){
 add_action('wp_enqueue_scripts', 'BV_Boilerplate_Scripts');
 function BV_Boilerplate_Scripts(){
 	wp_enqueue_style( 'BV-Boilerplate-style-reset', get_stylesheet_uri() );
-	wp_enqueue_style( 'BV-Boilerplate-style', get_template_directory_uri() . '/less/css/style.min.css' );
+	wp_enqueue_style( 'BV-Boilerplate-style', get_template_directory_uri() . '/scss/css/style.min.css' );
 	wp_enqueue_script('BV-Boilerplate-mainjs', get_template_directory_uri() . '/js/main.js', array(), false, true);
 }
 // end of BV_Boilerplate_Scripts
